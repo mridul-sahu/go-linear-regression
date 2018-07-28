e8eca52a3fb546a72faa039a109c52a1daff9b29
Mridul <mridulsahu01@gmail.com>
2018-07-28 16:27:57 +0530

    Implement Linear Regression

--- main.go
+++ main.go
@@ -10,13 +10,20 @@ import (
 	"gonum.org/v1/plot/vg/draw"
 	"golang.org/x/image/colornames"
 	"path/filepath"
+	"math"
+	"flag"
 )
 
 type xy struct {
 	x, y float64
 }
 
+var numIterations int
+
 func main() {
+	flag.IntVar(&numIterations, "n" ,1000, "number of iterations")
+	flag.Parse()
+
 	// Read the data from the file
 	xys, err := readData("data.txt")
 
@@ -26,7 +33,7 @@ func main() {
 
 
 	// Plot the data to the file
-	err = plotData(&xys, "output.png")
+	err = plotData(xys, "output.png")
 
 	if err != nil {
 		log.Fatalf("could not plot data: %v", err)
@@ -70,7 +77,7 @@ func readData(path string) (plotter.XYs, error) {
 	return xys, nil
 }
 
-func plotData(xys *plotter.XYs, path string) error {
+func plotData(xys plotter.XYs, path string) error {
 
 	f, err := os.Create(path)
 
@@ -105,6 +112,37 @@ func plotData(xys *plotter.XYs, path string) error {
 	// Add scatter to the plot
 	p.Add(s)
 
+	// Get line using linear regression
+	m, c := linearRegression(xys, 0.01, numIterations)
+
+	minX := math.MaxFloat64
+	maxX := -math.MaxFloat64
+
+	// Get minimum and maximum x values in our dataset to plot the line
+	for _, xy := range xys {
+
+		if xy.X < minX {
+			minX = xy.X
+		}
+
+		if xy.X > maxX {
+			maxX = xy.X
+		}
+	}
+
+	// line plotter using the minimum and maximum value of x
+	l, err := plotter.NewLine(plotter.XYs{
+		{minX, minX * m + c},
+		{maxX, maxX * m + c},
+	})
+
+	if err != nil {
+		return fmt.Errorf("could not create line: %v", err)
+	}
+
+	// Add line to plot
+	p.Add(l)
+
 	wt, err := p.WriterTo(256, 256, filepath.Ext(path)[1:]) // filepath.Ext(path)[1:] as need to ignore "." in ".png"
 
 	if err != nil {
@@ -122,4 +160,51 @@ func plotData(xys *plotter.XYs, path string) error {
 	}
 
 	return nil
+}
+
+func linearRegression(xys plotter.XYs, alpha float64, iterations int) (m, c float64) {
+
+	for i := 0; i < iterations; i++ {
+		// compute gradient
+		dm, dc := computeGradient(xys, m, c)
+
+		// Change values to minimize cost
+		m += -dm * alpha //Opposite to the direction of the gradient
+		c += -dc * alpha //Opposite to the direction of the gradient
+	}
+
+	log.Printf("final cost for (%f, %f): %f", m, c, computerCost(xys, m, c))
+
+	return m, c
+}
+
+func computerCost (xys plotter.XYs, m, c float64) float64 {
+	// cost = 1/N * sum((y - (m*x + c))^2)
+
+	sum := 0.0
+
+	for _, xy := range xys {
+		d := xy.Y - (m * xy.X + c)
+		sum += d * d
+	}
+
+	return sum/float64(len(xys))
+}
+
+func computeGradient(xys plotter.XYs, m, c float64) (dm, dc float64) {
+	// cost/dm = 2/N * sum( -x * (y - (m*x + c)))
+	// cost/dc = 2/N * sum(-1 * (y - (m*x + c)))
+
+	for _, xy := range xys {
+		d := xy.Y - (m * xy.X + c)
+		dm += -xy.X * d
+		dc += -d
+	}
+
+	n := float64(len(xys))
+
+	dm /= n
+	dc /= n
+
+	return 2*dm, 2*dc
 }
